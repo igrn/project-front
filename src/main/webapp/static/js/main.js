ccb87c53b4fce8a4828f62dc6b824489c3bab5ad
Igor Zelenin <iazelenin@icloud.com>
2026-09-05 16:41:41 +0400

    Add save and edit

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -41,15 +41,15 @@ window.addEventListener('DOMContentLoaded', () => {
                     const banned = item.banned ? 'Yes' : 'No';
                     const row = `<tr>
                             <td>${item.id}</td>
-                            <td>${item.name}</td>
-                            <td>${item.title}</td>
-                            <td>${item.race}</td>
-                            <td>${item.profession}</td>
+                            <td class="editable-name">${item.name}</td>
+                            <td class="editable-title">${item.title}</td>
+                            <td class="editable-race">${item.race}</td>
+                            <td class="editable-profession">${item.profession}</td>
                             <td>${item.level}</td>
                             <td>${birthday}</td>
-                            <td>${banned}</td>
+                            <td class="editable-banned">${banned}</td>
                             <td class="action-cell">
-                                <img src="/static/img/edit.png" alt="Edit" class="icon-btn edit-btn">
+                                <img src="/static/img/edit.png" alt="Edit" class="icon-btn edit-btn" data-id="${item.id}">
                             </td>
                             <td class="action-cell">
                                 <img src="/static/img/delete.png" alt="Delete" class="icon-btn delete-btn" data-id="${item.id}">
@@ -86,6 +86,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //Remove a player by clicking the delete button
     $('#table-body').on('click', '.delete-btn', function () {
+        //jQuery-specific method to fetch the 'data-id' attribute
         const playerId = $(this).data('id');
 
         if (confirm('Are you sure you want to delete this player?')) {
@@ -95,5 +96,71 @@ window.addEventListener('DOMContentLoaded', () => {
                 success: () => loadAdminPanel()
             });
         }
+    });
+
+    //Make fields editable by clicking the edit button
+    $('#table-body').on('click', '.edit-btn', function () {
+        const $currentRow = $(this).closest('tr');
+        $currentRow.find('.delete-btn').hide();
+        $(this).attr('src', '/static/img/save.png').attr('alt', 'Save')
+            .removeClass('edit-btn').addClass('save-btn');
+
+        //Text inputs
+        const makeEditableText = (selector) => {
+            const $cell = $currentRow.find(selector);
+            const currentText = $cell.text();
+            $cell.html(`<input type="text" value="${currentText}" style="width: 90%;">`);
+        };
+        makeEditableText('.editable-name');
+        makeEditableText('.editable-title');
+
+        //Enum selections
+        const makeEditableSelect = (selector, options) => {
+            const $cell = $currentRow.find(selector);
+            const currentValue = $cell.text();
+            const $select = $('<select style="width: 90%;"></select>');
+
+            $.each(options, (_index, optionValue) => {
+                const $option = $('<option></option>').val(optionValue).text(optionValue);
+
+                if (optionValue === currentValue) {
+                   $option.prop('selected', true);
+                }
+                $select.append($option);
+            });
+            $cell.html($select);
+        };
+        makeEditableSelect('.editable-race', races); //see index.html
+        makeEditableSelect('.editable-profession', professions);
+
+        const $bannedCell = $currentRow.find('.editable-banned');
+        const bannedValue = $bannedCell.text();
+        $bannedCell.html(`
+            <select>
+                <option value="false" ${bannedValue === 'No' ? 'selected' : ''}>No</option>
+                <option value="true" ${bannedValue === 'Yes' ? 'selected' : ''}>Yes</option>
+            </select>
+        `);
+    });
+
+    //Save all edits to the player info
+    $('#table-body').on('click', '.save-btn', function () {
+        const $currentRow = $(this).closest('tr');
+        const playerId = $(this).data('id');
+
+        const playerData = {
+            name: $currentRow.find('.editable-name input').val(),
+            title: $currentRow.find('.editable-title input').val(),
+            race: $currentRow.find('.editable-race select').val(),
+            profession: $currentRow.find('.editable-profession select').val(),
+            banned: $currentRow.find('.editable-banned select').val() === 'true'
+        };
+        $.ajax({
+            url: `/rest/players/${playerId}`,
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(playerData),
+            success: () => loadAdminPanel()
+        });
     });
 });
